3d333de1536b89bfe6dbafd25a5532e4339fffc0
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-12-06 14:32:48 -0800

    Home.js has a list of restaurants

--- project-2-foodie-api/src/App.js
+++ project-2-foodie-api/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Home from './Home';
 import Restaurant from './Restaurant';
 import NavBar from './NavBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
-      <Restaurant />
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurant/:id" element={<Restaurant />} />
+      </Routes>
     </>
   );
 }
