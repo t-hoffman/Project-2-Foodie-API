c24f75a400b2feee5dfb5cf9241a5510ee3c8b55
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-12-06 16:36:31 -0800

    Layout wrapped around components

--- project-2-foodie-api/src/NavBar.js
+++ project-2-foodie-api/src/NavBar.js
@@ -1,6 +1,37 @@
+import { Link } from 'react-router-dom';
+
 function NavBar () {
+    // const icon = document.getElementById('icon');
+    // const navLinks = document.getElementById('nav-links');
+    // icon.addEventListener('click', () => {
+    //     if (navLinks.style.display === 'block') {
+    //         navLinks.style.display = 'none';
+    //     } else {
+    //         navLinks.style.display = 'block';
+    //     }
+    // });
+
     return (
-        <>NavBar</>
+        <>
+        <div className="navbar">
+            <div className="logo">
+                <Link to="/">foodie</Link></div>
+            <div className="nav-search-container">
+                <div className="nav-search">
+                    <div className="nav-search-input">
+                        <input type="text" placeholder="search ..." /> 
+                    <a href="#" className="icon" id="icon"><i className="fa fa-bars"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="nav-links" id="nav-links">
+            <a href="#">Home</a>
+            <a href="#">Restaurants</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+        </div>
+        </>
     )
 }
 
