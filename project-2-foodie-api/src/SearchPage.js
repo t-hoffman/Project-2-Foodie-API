88c68869309891a300733ea65b4d7c3321571d6a
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-12-07 18:55:59 -0800

    Search MVP

--- project-2-foodie-api/src/SearchPage.js
+++ project-2-foodie-api/src/SearchPage.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+
+function RestaurantCard (props) {
+    const { id, name } = props;
+
+    return <><b><Link to={`/test/${id}`}>{name}</Link></b><br /></>
+}
 
 function SearchPage () {
-    const [geo, setGeo] = useState(null);
     const [list, setList] = useState(null);
 
     const params = useParams();
@@ -14,29 +19,40 @@ function SearchPage () {
         }
     };
 
-    useEffect (() => {
-        // fetch (cityURL, apiKey)
-        // .then (resp => resp.json())
-        // .then ((geo) => {
-        //     const locationID = geo.data[0].result_object.location_id;
-        //     const listURL = `https://travel-advisor.p.rapidapi.com/restaurants/list?location_id=${locationID}&currency=USD&lunit=km&limit=30&lang=en_US`;
-        //     fetch(listURL, apiKey)
-        //     .then (rs => rs.json())
-        //     .then (r => setList(r))
-        // })
-    }, []);
+    function fetchData (url, options, cb) {
+        fetch (url, options)
+        .then (resp => resp.json())
+        .then (json => cb (json))
+        .catch (err => console.log(err));
+    }
 
-    return (
-        <h1>{params.city}</h1>
-        if (list) {
-            console.log(list)
-            if (list) {
-                return list.data.map ((rest) => {
-                    return <div>{rest.name}</div>
-                })
+    useEffect (() => {
+        fetchData (cityURL, apiKey, (geo) => {
+            const locationID = geo.data[0].result_object.location_id;
+            const listURL = `https://travel-advisor.p.rapidapi.com/restaurants/list?location_id=${locationID}&currency=USD&lunit=km&limit=30&lang=en_US`;
+            fetchData (listURL, apiKey, setList)
+        })
+    }, [params]);
+
+    if (list) {
+        console.log(list)
+        return (
+            <>
+            <h1>{params.city}</h1>
+            {
+            list.data.map ((rest) => {
+                if (rest.name) {
+                    return <RestaurantCard 
+                                id={rest.location_id}
+                                name={rest.name} />
+                }
+            })
             }
-        }
-    )
+        </>
+        )
+    } else {
+        return <h1>Loading ...</h1>
+    }
 }
 
 export default SearchPage;
