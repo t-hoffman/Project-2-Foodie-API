a0d3334467965d55f48cb8e4b27f2725b1a6db50
Tyler Hoffman <tjhoffmn@gmail.com>
2022-12-11 19:57:10 -0800

    Yelp & TripAdvisor reviews on restaurant page

--- project-2-foodie-api/src/RestaurantPage.js
+++ project-2-foodie-api/src/RestaurantPage.js
@@ -41,16 +41,18 @@ function PhotoCarousel (props) {
             const locID = restaurant.location_id;
             const resultID = locID ? locID : restaurant.result_object.location_id;
             const photoURL = `https://travel-advisor.p.rapidapi.com/photos/list?location_id=${resultID}&currency=USD&limit=50&lang=en_US`;
-            fetchData (photoURL, travelAPI, setPics); console.log('RUN_API_PICS',pics)
+            fetchData (photoURL, travelAPI, (picData) => {
+                if (picData.length > 0) setPics(picData);
+                if (picData.length < 1) setPics('none');
+            }); console.log('RUN_API_PICS')
         }
     }
 
     if (restaurantData && restaurantData !== 'none') {
         fetchPhotos(props.data)
+    } else if (restaurantData === 'none') {
+        return <h2>Sorry, no photo results.</h2>
     }
-    // } else if (restaurantData === 'none') {
-    //     return <h2>Sorry, no photo results.</h2>
-    // }
 
     if (pics && pics !== 'none' && pics.length > 0) {
         return (<><h3>Photos ({restaurantData.name ? restaurantData.name : restaurantData.result_object.name} - {pics.length}):</h3>
@@ -69,7 +71,93 @@ function PhotoCarousel (props) {
         }
         </>)
     } else {
-        return pics && pics === 'none' || pics ? <h3>Sorry, no results for photos.</h3> : <h3>Loading photos ...</h3>
+        return !pics ? <h3>Loading photos ...</h3> : (pics && pics === 'none' && <h3>Sorry, no results for photos.</h3>)
+    }
+}
+
+function Reviews (props) {
+    const [yelpRev, setYelpRev] = useState(null);
+    const [tripRev, setTripRev] = useState(null);
+    const yelpRevURL = `https://api.yelp.com/v3/businesses/${props.yelpData.id}/reviews?limit=20&sort_by=yelp_sort`;
+
+    if (props.yelpData && !yelpRev) {
+        fetchYelpData(yelpRevURL, setYelpRev);
+    }
+
+    useEffect(() => {
+        if (props.tripData && !tripRev) {console.log('API_RUN_TRIP_REV')
+            const tripURL = `https://travel-advisor.p.rapidapi.com/reviews/list?location_id=${props.tripData.location_id}&limit=20&currency=USD&lang=en_US`;
+            fetchData(tripURL, travelAPI, setTripRev);
+        }
+    }, [tripRev]);
+
+    function tripReviews() {
+        if (tripRev) {
+            return tripRev.map((rev) => {
+                let userRating = [];
+                for (let i = 0; i < parseInt(rev.rating); i++) {
+                    userRating.push(i);
+                }
+
+                const profLink = rev.user.avatar.large.url ? rev.user.avatar.large.url : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'
+                
+                return (<>
+                <div class="review-title">
+                    <div className="avatar" style={{backgroundImage:`url(${profLink})`}}></div>
+                    <div class="user-info"><h4>{rev.user.username}</h4>
+                    {
+                        userRating.map((i) => {
+                            return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Five_Pointed_Star_Solid.svg/2175px-Five_Pointed_Star_Solid.svg.png" width="15px" />
+                        })
+                    }
+                    </div>
+                </div>
+                <div class="review-content">{rev.text}</div>
+                </>)
+            })
+        }
+    }
+
+    if (yelpRev && yelpRev.reviews) {
+        return (<>
+            <div className="review-header"><h2><sub style={{color:'#EF0B12'}}>yelp</sub> Reviews</h2></div>
+            {
+                yelpRev.reviews.map((rev) => {
+                    let userRating = [];
+                    for (let i = 0; i < rev.rating; i++) {
+                        userRating.push(i);
+                    }
+
+                    const profLink = rev.user.image_url ? rev.user.image_url : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'
+
+                    return (<>
+                    <div class="review-title">
+                        <div className="avatar" style={{backgroundImage:`url(${profLink})`}}></div>
+                        <div class="user-info"><h4>{rev.user.name}</h4>
+                        {
+                            userRating.map((i) => {
+                                return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Five_Pointed_Star_Solid.svg/2175px-Five_Pointed_Star_Solid.svg.png" width="15px" />
+                            })
+                        }
+                        </div>
+                    </div>
+                    <div class="review-content">{rev.text}</div>
+                    </>
+                    )
+                })
+            }
+            <div className="review-header"><h2><sub style={{color:'#32DFA2'}}>tripadvisor</sub> Reviews</h2></div>
+            {
+                tripReviews()
+            }
+        </>)
+    } else if (tripRev) {
+        return (<>
+            <div className="review-header"><h2><sub style={{color:'#32DFA2'}}>tripadvisor</sub> Reviews</h2></div>
+            {
+                tripReviews()
+            }
+        </>)
     }
 }
 
@@ -89,139 +177,15 @@ function RestaurantPage () {
         console.log('RUN_API_YELP')
     }, []);
 
-    // const yelpData = {
-    //     "id": "cal0Wpupxj9c_AV7WzDXsw",
-    //     "alias": "granville-west-hollywood-2",
-    //     "name": "GRANVILLE",
-    //     "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/znY6Wq1711cPvOtfKxWZZg/o.jpg",
-    //     "is_claimed": true,
-    //     "is_closed": false,
-    //     "url": "https://www.yelp.com/biz/granville-west-hollywood-2?adjust_creative=S2Ywzrk-Ow__n-DLVtG7dA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=S2Ywzrk-Ow__n-DLVtG7dA",
-    //     "phone": "+14245225161",
-    //     "display_phone": "(424) 522-5161",
-    //     "review_count": 2306,
-    //     "categories": [
-    //         {
-    //             "alias": "newamerican",
-    //             "title": "American (New)"
-    //         },
-    //         {
-    //             "alias": "cocktailbars",
-    //             "title": "Cocktail Bars"
-    //         }
-    //     ],
-    //     "rating": 4.5,
-    //     "location": {
-    //         "address1": "8701 Beverly Blvd",
-    //         "address2": null,
-    //         "address3": "",
-    //         "city": "West Hollywood",
-    //         "zip_code": "90048",
-    //         "country": "US",
-    //         "state": "CA",
-    //         "display_address": [
-    //             "8701 Beverly Blvd",
-    //             "West Hollywood, CA 90048"
-    //         ],
-    //         "cross_streets": ""
-    //     },
-    //     "coordinates": {
-    //         "latitude": 34.0771299,
-    //         "longitude": -118.38068
-    //     },
-    //     "photos": [
-    //         "https://s3-media2.fl.yelpcdn.com/bphoto/znY6Wq1711cPvOtfKxWZZg/o.jpg",
-    //         "https://s3-media4.fl.yelpcdn.com/bphoto/qGD8HfgyBHitlnbjyRNB7Q/o.jpg",
-    //         "https://s3-media1.fl.yelpcdn.com/bphoto/97EiWbz49hzIMI5dzBnvFQ/o.jpg"
-    //     ],
-    //     "price": "$$",
-    //     "hours": [
-    //         {
-    //             "open": [
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1130",
-    //                     "end": "2100",
-    //                     "day": 0
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1130",
-    //                     "end": "2100",
-    //                     "day": 1
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1130",
-    //                     "end": "2100",
-    //                     "day": 2
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1130",
-    //                     "end": "2100",
-    //                     "day": 3
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1130",
-    //                     "end": "2200",
-    //                     "day": 4
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1100",
-    //                     "end": "2200",
-    //                     "day": 5
-    //                 },
-    //                 {
-    //                     "is_overnight": false,
-    //                     "start": "1100",
-    //                     "end": "2100",
-    //                     "day": 6
-    //                 }
-    //             ],
-    //             "hours_type": "REGULAR",
-    //             "is_open_now": true
-    //         }
-    //     ],
-    //     "transactions": [
-    //         "restaurant_reservation",
-    //         "delivery",
-    //         "pickup"
-    //     ],
-    //     "messaging": {
-    //         "url": "https://www.yelp.com/raq/cal0Wpupxj9c_AV7WzDXsw?adjust_creative=S2Ywzrk-Ow__n-DLVtG7dA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=S2Ywzrk-Ow__n-DLVtG7dA#popup%3Araq",
-    //         "use_case_text": "Message the Business"
-    //     },
-    //     "special_hours": [
-    //         {
-    //             "date": "2022-12-25",
-    //             "is_closed": true,
-    //             "start": null,
-    //             "end": null,
-    //             "is_overnight": null
-    //         },
-    //         {
-    //             "date": "2022-12-26",
-    //             "is_closed": true,
-    //             "start": null,
-    //             "end": null,
-    //             "is_overnight": null
-    //         }
-    //     ]
-    // };
-
-    //console.log(yelpData);
-
     if (yelpData && data) {
-        console.log(data)
         let categories = '';
         if (yelpData.categories) {
             yelpData.categories.forEach((cat, i) => {
                 categories += (i === yelpData.categories.length-1 ? cat.title : cat.title + ', ');
             })
         }
+
+        const tripRatingInfo = data !== 'none' ? <div style={{padding:'12px 0 0 10px'}}>{yelpData.review_count.toLocaleString('en-US')} reviews<br /> <p style={{paddingTop:'5px'}}>{data.num_reviews} reviews, {data.ranking}</p></div> : ''
     return (
     <>
         <div className="title-container">
@@ -237,14 +201,19 @@ function RestaurantPage () {
             <sub style={{color:'#32DFA2'}}>tripadvisor <img src="https://w7.pngwing.com/pngs/70/397/png-transparent-tripadvisor-logo-hotel-hotel-miscellaneous-beach-logo-thumbnail.png" height="20px" /></sub>
             </div>
             <div style={{padding:'12px 0 0 10px'}}>{yelpData.rating}<br /> <p style={{paddingTop:'5px'}}>{data.rating}</p></div>
-            <div style={{padding:'12px 0 0 10px'}}>{yelpData.review_count.toLocaleString('en-US')} reviews<br /> <p style={{paddingTop:'5px'}}>{data.num_reviews} reviews, {data.ranking}</p></div>
+            {tripRatingInfo}
         </div>
         <div style={{paddingTop:'15px'}}>
-        {data.description}
-        <div style={{paddingTop: '15px'}}>
-        {/* GRANVILLE is a collection of modern-casual restaurants specializing in wholesome hand-crafted recipes and libations. With warm hospitality, good vibes, globally-inspired food and music - we are a culture, not a concept. GRANVILLE can be described as an experiment in humanity. We believe that fostering a culture of love, gratitude and integrity is the way of life, and business. We support local, organic and certified humane practices whenever possible and make our food from scratch daily. We offer lunch, dinner, weekend breakfast and full bar in a casual yet tasteful environment. */}
+            {data.description}
         </div>
-        <PhotoCarousel data={data} />
+        <div style={{paddingTop:'15px'}}>
+            <PhotoCarousel data={data} />
+        </div>
+        <div>
+            <Reviews 
+                yelpData={yelpData}
+                tripData={data}
+            />
         </div>
     </>)
     } else {
