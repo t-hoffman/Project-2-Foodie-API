5f61f4e9a665a408477b66bfdcbfbdf314a10389
Tyler Hoffman <tjhoffmn@gmail.com>
2022-12-12 14:55:39 -0800

    PROJECT 2 COMPLETE

--- layout.js
+++
@@ -1,9 +0,0 @@
-const icon = document.querySelector('.icon');
-const navLinks = document.querySelector('.nav-links');
-icon.addEventListener('click', () => {
-    if (navLinks.style.display === 'block') {
-        navLinks.style.display = 'none';
-    } else {
-        navLinks.style.display = 'block';
-    }
-});
