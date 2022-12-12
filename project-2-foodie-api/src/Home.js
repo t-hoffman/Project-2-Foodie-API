44ba0c98a5b619e86c6a669a2af8afe34fb3495b
Tyler Hoffman <tjhoffmn@gmail.com>
2022-12-11 22:13:38 -0800

    Getting ready for Netlify deployment

--- project-2-foodie-api/src/Home.js
+++ project-2-foodie-api/src/Home.js
@@ -3,7 +3,6 @@ import React from "react"
 import { useState, useEffect } from "react";
 // import { useParams } from "react-router";
 import { Link } from 'react-router-dom';
-import Carousel from "./carousel.js";
 const proxyURL = 'https://thoffman-corsproxy.herokuapp.com/';
 const LA_URL = 'https://api.yelp.com/v3/businesses/search?location=los%20angeles%2C%20ca&term=restaurants&sort_by=best_match&limit=20';
 const apiKey = process.env.REACT_APP_YELP_API_KEY;
