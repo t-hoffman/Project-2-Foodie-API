2f5cc2326828a7b016de50f115b965385667671c
chengu <jeddycheng@gmail.com>
2022-12-20 14:52:41 -0800

    updated

--- project-2-foodie-api/src/App.js
+++ project-2-foodie-api/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './Home';
-
 import NavBar from './NavBar';
 import SearchPage from './SearchPage_Yelp';
 import RestaurantPage from './RestaurantPage';
@@ -16,7 +15,6 @@ function App() {
       <div className="content">
       <Routes>
         <Route path="/" element={<Home />} />
-        
         <Route path="/search/:city" element={<SearchPage />} />
         <Route path="/restaurantpage/:id" element={<RestaurantPage />} />
         <Route path="/restaurantpage/:id/:name" element={<RestaurantPage />} />
